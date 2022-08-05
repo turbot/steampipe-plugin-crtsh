c0051dc37ce218941289b3f0511845b1eb623ebf
Nathan Wallace <nathan@turbot.com>
2022-08-05 15:21:59 -0400

    Remove crtsh_log_operator table, use crtsh_log instead

--- crtsh/plugin.go
+++ crtsh/plugin.go
@@ -12,12 +12,11 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 		Name:             "steampipe-plugin-crtsh",
 		DefaultTransform: transform.FromGo(),
 		TableMap: map[string]*plugin.Table{
-			"crtsh_ca":           tableCrtshCa(),
-			"crtsh_ca_issuer":    tableCrtshCaIssuer(),
-			"crtsh_certificate":  tableCrtshCertificate(),
-			"crtsh_log":          tableCrtshLog(),
-			"crtsh_log_entry":    tableCrtshLogEntry(),
-			"crtsh_log_operator": tableCrtshLogOperator(),
+			"crtsh_ca":          tableCrtshCa(),
+			"crtsh_ca_issuer":   tableCrtshCaIssuer(),
+			"crtsh_certificate": tableCrtshCertificate(),
+			"crtsh_log":         tableCrtshLog(),
+			"crtsh_log_entry":   tableCrtshLogEntry(),
 		},
 	}
 	return p
